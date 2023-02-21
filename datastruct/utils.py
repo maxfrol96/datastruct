12c77a55fb646692419a1ed7ec4cb6a3635479f3
maxfrol96 <maxfrol96@mail.ru>
2023-02-21 13:28:21 +0300

    dz 2

--- datastruct/utils.py
+++ datastruct/utils.py
@@ -13,3 +13,26 @@ class Stack:
         new_node = Node(data)
         new_node.next_node = self.top
         self.top = new_node
+
+    def pop(self):
+        pop_node = self.top
+        pop_data = pop_node.data
+        self.top = pop_node.next_node
+        return pop_data
+
+stack = Stack()
+# stack.push('data1')
+# stack.push('data2')
+# stack.push('data3')
+# print(stack.top.data)
+# print(stack.top.next_node.data)
+# print(stack.top.next_node.next_node.data)
+# print(stack.top.next_node.next_node.next_node)
+stack = Stack()
+stack.push('data1')
+stack.push('data2')
+data = stack.pop()
+
+# теперь последний элемента содержит данные data1
+print(stack.top.data)
+print(data)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -8,8 +8,9 @@ class TestNode(unittest.TestCase):
         stack = Stack()
         stack.push('data1')
         stack.push('data2')
-        stack.push('data3')
-        self.assertEqual(stack.top.data, 'data3')
+        self.assertEqual(stack.top.data, 'data2')
+        self.assertEqual(stack.pop(), 'data2')
+
 
 
 
