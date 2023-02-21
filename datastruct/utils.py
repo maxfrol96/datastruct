8c45ae0a53850ab68d9f5eab2838198cb2a69aeb
maxfrol96 <maxfrol96@mail.ru>
2023-02-21 13:29:46 +0300

    dz 2

--- datastruct/utils.py
+++ datastruct/utils.py
@@ -20,19 +20,3 @@ class Stack:
         self.top = pop_node.next_node
         return pop_data
 
-stack = Stack()
-# stack.push('data1')
-# stack.push('data2')
-# stack.push('data3')
-# print(stack.top.data)
-# print(stack.top.next_node.data)
-# print(stack.top.next_node.next_node.data)
-# print(stack.top.next_node.next_node.next_node)
-stack = Stack()
-stack.push('data1')
-stack.push('data2')
-data = stack.pop()
-
-# теперь последний элемента содержит данные data1
-print(stack.top.data)
-print(data)
