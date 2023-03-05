26f69a809e92bd4c77d482ba317a2e611e0091bf
maksfrol96 <maxfrol96@mail.ru>
2023-03-05 18:12:56 +0300

    dz 4

--- datastruct/custom_queue.py
+++ datastruct/custom_queue.py
@@ -20,6 +20,25 @@ class Queue:
             self.tail.next_node = new_node
             self.tail = new_node
 
+    def dequeue(self):
+        deqeueued_node = self.head
+        if self.head == None:
+            return None
+        else:
+            self.head = self.head.next_node
+            return deqeueued_node.data
+
+queue = Queue()
+queue.enqueue('data1')
+queue.enqueue('data2')
+queue.enqueue('data3')
+# print(queue.head.data)
+# print(queue.head.next_node.data)
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.dequeue())
+print(queue.head)
+
 
 
 
